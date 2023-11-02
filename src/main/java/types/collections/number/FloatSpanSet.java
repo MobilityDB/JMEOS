6e96401485bf48843ce69294d3be019fa5f4b54c
Nidhal <nidhal.mareghni@ulb.be>
2023-11-02 04:52:52 +0100

    Implementation of FloatSpanSet and IntSpanSet

--- src/main/java/types/collections/number/FloatSpanSet.java
+++ src/main/java/types/collections/number/FloatSpanSet.java
@@ -1,6 +1,7 @@
 package types.collections.number;
 import functions.functions;
 import jnr.ffi.Pointer;
+import types.collections.base.Base;
 import types.collections.base.SpanSet;
 
 /**
@@ -92,9 +93,378 @@ public class FloatSpanSet extends SpanSet<Float> implements Number{
 
     /** ------------------------- Accessors ------------------------------------- */
 
-    
+
+    /**
+     * Returns the width of the spanset. By default, i.e., when the second
+     *         argument is False, the function takes into account the gaps within,
+     *         i.e., returns the sum of the widths of the spans within.
+     *         Otherwise, the function returns the width of the spanset ignoring
+     *         any gap, i.e., the width from the lower bound of the first span to
+     *         the upper bound of the last span.
+     * <p>
+     *         MEOS Functions:
+     *             <li>spanset_width</li>
+     *
+     * @param ignore_gaps Whether to take into account potential gaps in
+     *      *             the spanset.
+     * @return A `float` representing the duration of the spanset
+     */
+    public float width(boolean ignore_gaps){
+        return (float) functions.spanset_width(this._inner);
+    }
+
+    /**
+     * Returns the first span in "this".
+     * <p>
+     *         MEOS Functions:
+     *             <li>spanset_start_span</li>
+     *
+     * @return A {@link FloatSpan} instance
+     */
+    public FloatSpan start_span(){
+        return new FloatSpan(super.start_span().get_inner());
+    }
 
 
+    /**
+     * Returns the last span in "this".
+     * <p>
+     *         MEOS Functions:
+     *             <li>spanset_end_span</li>
+     *
+     * @return A {@link FloatSpan} instance
+     */
+    public FloatSpan end_span(){
+        return new FloatSpan(super.end_span().get_inner());
+    }
+
+
+    /**
+     * Returns the n-th span in "this".
+     * <p>
+     *         MEOS Functions:
+     *             <li>spanset_span_n</li>
+     *
+     * @return A {@link FloatSpan} instance
+     */
+    public FloatSpan span_n(int n){
+        return new FloatSpan(super.span_n(n).get_inner());
+    }
+
+    /** ------------------------- Transformations ------------------------------- */
+
+
+    /**
+     * Return a new "FloatSpanSet" with the lower and upper bounds shifted by
+     *         "delta".
+     *
+     *  <p>
+     *         MEOS Functions:
+     *             <li>floatspanset_shift_scale</li>
+     *
+     *
+     * @param delta The value to shift by
+     * @return A new {@link FloatSpanSet} instance
+     */
+    public FloatSpanSet shif(int delta){
+        return this.shift_scale(delta,0);
+    }
+
+    /**
+     * Return a new "FloatSpanSet" with the lower and upper bounds scaled so
+     *         that the width is "width".
+     *
+     *  <p>
+     *         MEOS Functions:
+     *             <li>floatspanset_shift_scale</li>
+     *
+     * @param width The new width
+     * @return a new {@link FloatSpanSet} instance
+     */
+    public FloatSpanSet scale(int width){
+        return this.shift_scale(0,width);
+    }
+
+    /**
+     * Return a new "FloatSpanSet" with the lower and upper bounds shifted by
+     *         "delta" and scaled so that the width is "width".
+     *
+     *  <p>
+     *         MEOS Functions:
+     *             <li>floatspanset_shift_scale</li>
+     *
+     * @param delta The value to shift by
+     * @param width v
+     * @return a new {@link FloatSpanSet} instance
+     */
+    /*
+    public FloatSpanSet shift_scale(int delta, int width){
+        return new FloatSpanSet(functions.floatspanset_shift_scale(this._inner,delta,width));
+    }
+
+     */
+
+
+
+    /** ------------------------- Topological Operations -------------------------------- */
+
+
+    /**
+     * Returns whether "this" is adjacent to "other". That is, they share
+     *         a bound but only one of them contains it.
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>adjacent_spanset_span</li>
+     *             <li>adjacent_spanset_spanset</li>
+     *             <li>adjacent_floatspanset_float</li>
+     *
+     * @param other object to compare with
+     * @return True if adjacent, False otherwise
+     * @throws Exception
+     */
+    public boolean is_adjacent(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.adjacent_floatspanset_float(this._inner, (float) other);
+        }
+        else{
+            return super.is_adjacent((Base)other);
+        }
+    }
+
+    /**
+     * Returns whether "this" contains "content".
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>contains_spanset_span</li>
+     *             <li>contains_spanset_spanset</li>
+     *             <li>contains_floatspanset_float</li>
+     *
+     * @param other object to compare with
+     * @return True if contains, False otherwise
+     * @throws Exception
+     */
+    public boolean contains(Object other) throws Exception {
+        if (other instanceof Integer){
+            return functions.contains_floatspanset_float(this._inner, (float) other);
+        }
+        else{
+            return super.contains((Base)other);
+        }
+    }
+
+
+    /**
+     * Returns whether "this" and the bounding period of "other" is the
+     *         same.
+     *
+     * <p>
+     *         MEOS Functions:
+     *             <li>same_period_temporal</li>
+     *
+     * @param other object to compare with
+     * @return True if equal, False otherwise
+     * @throws Exception
+     */
+    public boolean is_same(Object other) throws Exception {
+        if (other instanceof Integer){
+            return functions.spanset_eq(this._inner,functions.float_to_floatspanset((float) other));
+        }
+        else{
+            return super.is_same((Base)other);
+        }
+    }
+
+
+
+    /** ------------------------- Position Operations --------------------------- */
+
+    /**
+     * Returns whether "this" is strictly left of "other". That is,
+     *         "this" ends before "other" starts.
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>left_span_span</li>
+     *             <li>left_span_spanset</li>
+     *             <li>left_floatspan_float</li>
+     *
+     * @param other object to compare with
+     * @return True if left, False otherwise
+     * @throws Exception
+     */
+    public boolean is_left(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.left_floatspanset_float(this._inner,(float) other);
+        }
+        else{
+            return super.is_left((Base)other);
+        }
+    }
+
+
+    /**
+     * Returns whether "this" is left "other" allowing overlap. That is,
+     *         "this" ends before "other" ends (or at the same value).
+     *
+     * <p>
+     *
+     *         MEOS Functions:
+     *             <li>overleft_span_span</li>
+     *             <li>overleft_span_spanset</li>
+     *             <li>overleft_floatspan_float</li>
+     *
+     * @param other object to compare with
+     * @return True if before, False otherwise
+     * @throws Exception
+     */
+    public boolean is_over_or_left(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.overleft_floatspanset_float(this._inner,(float) other);
+        }
+        else{
+            return super.is_over_or_left((Base)other);
+        }
+    }
+
+
+    /**
+     * Returns whether "this" is strictly right "other". That is, "this"
+     *         starts after "other" ends.
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>right_span_span</li>
+     *             <li>right_span_spanset</li>
+     *             <li>right_floatspan_float</li>
+     *
+     * @param other object to compare with
+     * @return True if right, False otherwise
+     * @throws Exception
+     */
+    public boolean is_right(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.right_floatspanset_float(this._inner,(float) other);
+        }
+        else{
+            return super.is_right((Base)other);
+        }
+    }
+
+
+
+    /**
+     *  Returns whether "this" is right "other" allowing overlap. That is,
+     *         "this" starts after "other" starts (or at the same value).
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>overright_spanset_span</li>
+     *             <li>overright_spanset_spanset</li>
+     *             <li>overright_floatspanset_float</li>
+     *
+     * @param other object to compare with
+     * @return True if overlapping or after, False otherwise
+     * @throws Exception
+     */
+    public boolean is_over_or_right(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.overright_floatspanset_float(this._inner,(float) other);
+        }
+        else{
+            return super.is_over_or_right((Base)other);
+        }
+    }
+
+
+
+    /** ------------------------- Distance Operations --------------------------- */
+
+    /**
+     * Returns the distance between "this" and "other".
+     *
+     *   <p>
+     *         MEOS Functions:
+     *             <li>distance_spanset_span</li>
+     *             <li>distance_spanset_spanset</li>
+     *             <li>distance_floatspanset_float</li>
+     *
+     * @param other object to compare with
+     * @return A float value
+     * @throws Exception
+     */
+    public float distance(Object other) throws Exception {
+        if (other instanceof Integer){
+            //return functions.distance_floatspanset_float(this._inner,(float) other);
+        }
+        else{
+            return super.distance((Base)other);
+        }
+    }
+
+
+    /** ------------------------- Set Operations -------------------------------- */
+
+
+    //TODO: intersection
+
+
+    /**
+     * Returns the difference of "this" and "other".
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>minus_spanset_span</li>
+     *             <li>minus_spanset_spanset</li>
+     *             <li>minus_floatspanset_float</li>
+     *
+     * @param other object to diff with
+     * @return  A {@link FloatSpanSet} instance.
+     * @throws Exception
+     */
+    public FloatSpanSet minus(Object other) throws Exception {
+        Pointer result;
+        if ((other instanceof Integer) || (other instanceof Float)){
+            //result = functions.minus_floatspanset_float(this._inner, (float) other);
+        }
+        else{
+            //result = super.minus((Base) other)
+        }
+        return new FloatSpanSet(result);
+    }
+
+
+    /**
+     * Returns the union of "this" and "other".
+     *
+     *  <p>
+     *
+     *         MEOS Functions:
+     *             <li>union_floatspanset_float</li>
+     *             <li>union_spanset_spanset</li>
+     *             <li>union_spanset_span</li>
+     *
+     * @param other object to merge with
+     * @return A {@link FloatSpanSet} instance.
+     */
+    public FloatSpanSet union(Object other){
+
+        if ((other instanceof Integer) || (other instanceof Float)){
+            //result = functions.union_floatspanset_float(this._inner, (float) other);
+        }
+        else{
+            //result = super.minus((Base) other)
+        }
+        return new FloatSpanSet(result);
+
+    }
 
 
 
