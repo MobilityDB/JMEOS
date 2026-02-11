0c78a2f331279c9f279c51f32b6268fe6413aa85
Jashan Singh <jashan271004@gmail.com>
2026-02-11 12:06:08 +0100

    add: renamed Hello_World_01 into Hello_World & created Hello_World_Geodetic

--- src/main/java/examples/Hello_World_Geodetic.java
+++ src/main/java/examples/Hello_World_Geodetic.java
@@ -2,15 +2,15 @@ package examples;
 
 import functions.error_handler;
 import functions.error_handler_fn;
-import types.basic.tpoint.tgeom.TGeomPointInst;
-import types.basic.tpoint.tgeom.TGeomPointSeq;
-import types.basic.tpoint.tgeom.TGeomPointSeqSet;
+import types.basic.tpoint.tgeog.TGeogPointInst;
+import types.basic.tpoint.tgeog.TGeogPointSeq;
+import types.basic.tpoint.tgeog.TGeogPointSeqSet;
 
 import java.sql.SQLException;
 
 import static functions.functions.*;
 
-public class Hello_World_01 {
+public class Hello_World_Geodetic {
 
 
     public static void main(String[] args) throws SQLException {
@@ -21,6 +21,7 @@ public class Hello_World_01 {
         meos_initialize_timezone("UTC");
         meos_initialize_error_handler(errorHandler);
 
+        String srs = "EPSG:4326";
         String inst_wkt = "POINT(1 1)@2000-01-01";
         String seq_disc_wkt = "{POINT(1 1)@2000-01-01, POINT(2 2)@2000-01-02}";
         String seq_linear_wkt = "[POINT(1 1)@2000-01-01, POINT(2 2)@2000-01-02]";
@@ -31,16 +32,16 @@ public class Hello_World_01 {
                 + "[POINT(3 3)@2000-01-03, POINT(3 3)@2000-01-04]}";
 
 
-        TGeomPointInst inst = new TGeomPointInst(inst_wkt);
-        TGeomPointSeq seq_disc = new TGeomPointSeq(seq_disc_wkt);
-        TGeomPointSeq seq_linear = new TGeomPointSeq(seq_linear_wkt);
-        TGeomPointSeq seq_step = new TGeomPointSeq(seq_step_wkt);
-        TGeomPointSeqSet ss_linear = new TGeomPointSeqSet(ss_linear_wkt);
-        TGeomPointSeqSet ss_step = new TGeomPointSeqSet(ss_step_wkt);
+        TGeogPointInst inst = new TGeogPointInst(inst_wkt);
+        TGeogPointSeq seq_disc = new TGeogPointSeq(seq_disc_wkt);
+        TGeogPointSeq seq_linear = new TGeogPointSeq(seq_linear_wkt);
+        TGeogPointSeq seq_step = new TGeogPointSeq(seq_step_wkt);
+        TGeogPointSeqSet ss_linear = new TGeogPointSeqSet(ss_linear_wkt);
+        TGeogPointSeqSet ss_step = new TGeogPointSeqSet(ss_step_wkt);
 
 
         /* Convert result to MF-JSON */
-        String inst_mfjson = inst.as_mfjson( true, 3, 6, null);
+        String inst_mfjson = inst.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "--------------------\n" +
                 "| Temporal Instant |\n" +
@@ -49,7 +50,7 @@ public class Hello_World_01 {
                 "----\n%s\n\n" +
                 "MF-JSON:\n" +
                 "--------\n%s\n", inst_wkt, inst_mfjson);
-        String seq_disc_mfjson = seq_disc.as_mfjson( true, 3, 6, null);
+        String seq_disc_mfjson = seq_disc.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "-------------------------------------------------\n" +
                 "| Temporal Sequence with Discrete Interpolation |\n" +
@@ -58,7 +59,7 @@ public class Hello_World_01 {
                 "----\n%s\n\n" +
                 "MF-JSON:\n" +
                 "--------\n%s\n", seq_disc_wkt, seq_disc_mfjson);
-        String seq_linear_mfjson = seq_linear.as_mfjson( true, 3, 6, null);
+        String seq_linear_mfjson = seq_linear.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "-----------------------------------------------\n" +
                 "| Temporal Sequence with Linear Interpolation |\n" +
@@ -67,7 +68,7 @@ public class Hello_World_01 {
                 "----\n%s\n\n" +
                 "MF-JSON:\n" +
                 "--------\n%s\n", seq_linear_wkt, seq_linear_mfjson);
-        String seq_step_mfjson = seq_step.as_mfjson( true, 3, 6, null);
+        String seq_step_mfjson = seq_step.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "--------------------------------------------\n" +
                 "| Temporal Sequence with Step Interpolation |\n" +
@@ -76,7 +77,7 @@ public class Hello_World_01 {
                 "----\n%s\n\n" +
                 "MF-JSON:\n" +
                 "--------\n%s\n", seq_step_wkt, seq_step_mfjson);
-        String ss_linear_mfjson = ss_linear.as_mfjson( true, 3, 6, null);
+        String ss_linear_mfjson = ss_linear.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "---------------------------------------------------\n" +
                 "| Temporal Sequence Set with Linear Interpolation |\n" +
@@ -85,7 +86,7 @@ public class Hello_World_01 {
                 "----\n%s\n\n" +
                 "MF-JSON:\n" +
                 "--------\n%s\n", ss_linear_wkt, ss_linear_mfjson);
-        String ss_step_mfjson = ss_step.as_mfjson( true, 3, 6, null);
+        String ss_step_mfjson = ss_step.as_mfjson( true, 3, 6, srs);
         System.out.printf("\n" +
                 "------------------------------------------------\n" +
                 "| Temporal Sequence Set with Step Interpolation |\n" +
