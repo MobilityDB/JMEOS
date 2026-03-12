14834dc36e88e5d5931836a47aff97cbddc0f1f8
Jashan Singh <jashan271004@gmail.com>
2026-03-12 16:22:32 +0100

    fix: modified pointer initialization in TPointInst & TInstant constructors

--- src/main/java/types/basic/tpoint/TPointInst.java
+++ src/main/java/types/basic/tpoint/TPointInst.java
@@ -35,7 +35,7 @@ public abstract class TPointInst extends TInstant<Point> implements TPoint{
 	 */
 	public TPointInst(String value){
 		super(value);
-		this.inner = createStringInner(value);
+		this.inner = super.get_inner();
 	}
 
 	public abstract Pointer createStringInner(String str);
--- src/main/java/types/temporal/TInstant.java
+++ src/main/java/types/temporal/TInstant.java
@@ -28,7 +28,11 @@ public abstract class TInstant<V extends Serializable> extends Temporal<V> {
 
 	public TInstant(String str){
 		super(str);
-		this._inner = createStringInner(str);
+		this._inner = super.getInner();
+	}
+
+	public Pointer get_inner() {
+		return _inner;
 	}
 
 	public abstract Pointer createStringInner(String str);
