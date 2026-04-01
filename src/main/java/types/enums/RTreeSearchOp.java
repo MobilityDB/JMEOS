1763023a35f906ed68630e4ac68fcbb032d212eb
Jashan Singh <jashan271004@gmail.com>
2026-04-01 10:20:39 +0200

    add: new enum class for RTree search operations

---
+++ src/main/java/types/enums/RTreeSearchOp.java
@@ -0,0 +1,17 @@
+package types.enums;
+
+public enum RTreeSearchOp {
+    RTREE_OVERLAPS(0),    /**< Find stored boxes that overlap the query */
+    RTREE_CONTAINS(1),    /**< Find stored boxes that contain the query */
+    RTREE_CONTAINED_BY(2); /**< Find stored boxes contained by the query */
+
+    private final int value;
+
+    RTreeSearchOp(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
